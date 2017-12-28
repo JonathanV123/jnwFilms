7d2319548d5261b22a547c48f7330e55b0f8414e
Jonathan <jvoxland@gmail.com>
2017-12-28 12:42:23 -0800

    added animations and changed various css and html elements based on client feedback

--- src/js/app.js
+++ src/js/app.js
@@ -1,12 +1,12 @@
 $(document).ready(function () {
     $('#fullpage').fullpage({
         //Navigation
-        menu: '#menu',
+        menu: '#myMenu',
         lockAnchors: false,
-        anchors: ['firstPage', 'secondPage', 'thirdpage'],
+        anchors: ['firstPage', 'secondPage', 'thirdpage', 'fourthPage'],
         navigation: true,
         navigationPosition: 'right',
-        navigationTooltips: ['Scroll Down / Swipe Down', 'Film', 'Film cont..', 'Television', 'Contact Me'],
+        navigationTooltips: ['Welcome', 'Film', 'Film', 'Television', 'Contact Me'],
         showActiveTooltip: true,
         slidesNavigation: true,
         slidesNavPosition: 'bottom',
@@ -32,7 +32,7 @@ $(document).ready(function () {
         resetSliders: false,
         fadingEffect: false,
         normalScrollElements: '#element1, .element2',
-        scrollOverflow: false,
+        scrollOverflow: true,
         scrollOverflowReset: false,
         scrollOverflowOptions: null,
         touchSensitivity: 15,
@@ -51,7 +51,7 @@ $(document).ready(function () {
         paddingTop: '3em',
         paddingBottom: '10px',
         fixedElements: '#header, .footer',
-        responsiveWidth: 0,
+        responsiveWidth: 950,
         responsiveHeight: 0,
         responsiveSlides: false,
         parallax: false,
@@ -60,19 +60,68 @@ $(document).ready(function () {
         //Custom selectors
         sectionSelector: '.section',
         slideSelector: '.slide',
-
         lazyLoading: true,
 
         //events
-        onLeave: function (index, nextIndex, direction) { },
-        afterLoad: function (anchorLink, index) { },
         afterRender: function () { },
         afterResize: function () { },
         afterResponsive: function (isResponsive) { },
         afterSlideLoad: function (anchorLink, index, slideAnchor, slideIndex) { },
-        onSlideLeave: function (anchorLink, index, slideIndex, direction, nextSlideIndex) { }
+        onSlideLeave: function (anchorLink, index, slideIndex, direction, nextSlideIndex) { },
+        onLeave: function (index, nextIndex, direction) {
+            var leavingSection = $(this);
+
+            //after leaving section 2
+            if (index == 1 && direction == 'down') {
+                var loadedSection = $(this);
+                console.log("Cool");
+                var projectDescription = document.querySelectorAll('.film_project_completed');
+                console.log(projectDescription)
+                Array.prototype.forEach.call(projectDescription, function (elements, index) {
+                    (function (index) {
+                        setTimeout(function () {
+                            $(elements).addClass('fade_in_animation');
+                        }, index * 200);
+                    })(index);
+                });
+
+            }
+
+            else if (index == 2 && direction == 'up') {
+                console.log("Going to section 1!");
+                console.log(index);
+
+            }
+            else if (index == 2 && direction == 'down') {
+                console.log("Going to section 3!");
+                console.log(index);
+                var projectDescription = document.querySelectorAll('.film_project');
+                Array.prototype.forEach.call(projectDescription, function (elements, index) {
+                    (function (index) {
+                        setTimeout(function () {
+                            $(elements).addClass('fade_in_animation');
+                        }, index * 200);
+                    })(index);
+                });
+            }
+            else if (index == 3 && direction == 'up') {
+                console.log("Going to section 2!");
+                console.log(index);
+            }
+        },
+        afterLoad: function (anchorLink, index) {
+            var loadedSection = $(this);
+            if (anchorLink == 'thirdPage') {
+                console.log("third page");
+
+            }
+        }
+
     });
+    function animate_fade_in(){
+
+    }
     // Fade out load screen
-    $("#loadScreen").fadeOut(6600);
-    $("#rain_bg").delay(4500).fadeOut(5600);
+    $("#loadScreen").fadeOut(3000);
 });
+
